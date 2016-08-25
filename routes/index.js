775891babe86d09adf6a02e507132d27c05a9c74
Dave Harned <dharned@gmail.com>
2016-08-24 22:45:23 -0500

    add post service to db

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,7 @@ router.get('/', function(req, res, next) {
   });
 });
 
+/* GET all users page */
 router.get('/userlist', function(req, res) {
 	var db = req.db;
 	var collection = db.get('usercollection');
@@ -19,5 +20,36 @@ router.get('/userlist', function(req, res) {
 		});
 	});
 });
+
+/* GET new user page */
+router.get('/newuser', function(req, res) {
+	res.render('newuser', { title: 'Add New User'});
+});
+
+/* POST to Add User service */
+router.post('/adduser', function(req, res) {
+	// Set internal DB variable
+	var db = req.db;
+
+	// Get form values
+	var userName = req.body.username;
+	var userEmail = req.body.useremail;
+
+	// Set collection
+	var collection = db.get('usercollection');
+
+	//Submit to DB
+	collection.insert({
+		"username" : userName,
+		"email" : userEmail
+	}, function(err, doc) {
+		if (err) {
+			res.send("There was a problem adding info to DB");
+		} else {
+			res.redirect('userlist');
+		}
+	});
+});
+
 
 module.exports = router;
