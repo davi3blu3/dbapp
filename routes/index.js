fe20e8460ea2a6837fe039af78dbb7b44f82548a
Dave Harned <dharned@gmail.com>
2016-08-24 21:59:55 -0500

    connect database and output data

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+  	title: 'Express',
+  	message: 'Hello World, Pumpkin',
+  	response: 'I love you, Honeybunny'
+  });
+});
+
+router.get('/userlist', function(req, res) {
+	var db = req.db;
+	var collection = db.get('usercollection');
+	collection.find({},{},function(e, docs) {
+		res.render('userlist', {
+			'userlist' : docs
+		});
+	});
 });
 
 module.exports = router;
